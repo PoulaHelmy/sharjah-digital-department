3ddde1342ddfdae6bd2f93766c7320512a9cc944
PoulaHelmy <bolahelmy2016@gmail.com>
2025-06-21 18:05:09 +0300

    :lipstick: implement responsive header with mobile menu toggle and enhance navigation structure

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,13 +1,44 @@
 import {Component} from '@angular/core';
 import {HeaderNavData} from '@core/constants';
+import {TranslatePipe} from '@ngx-translate/core';
+import {IconComponent} from '@shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe,
+    IconComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   headerNavData = HeaderNavData;
+  navbarOpen = false;
+  menuOpen = false;
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // A function to toggle dark mode (add your logic here)
+  toggleDarkMode() {
+    console.log('Dark mode toggled');
+    // Example: document.body.classList.toggle('dark-theme');
+  }
+
+  // A function to switch the language (add your logic here)
+  switchLanguage() {
+    console.log('Language switched');
+  }
 }
